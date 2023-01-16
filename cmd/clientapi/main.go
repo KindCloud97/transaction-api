cdac4dbee7a445411e45abbb66dbc0dc3c2c4f0c
KindCloud97 <alexproskurov97@gmail.com>
2023-01-16 17:23:34 +0000

    add client

---
+++ cmd/clientapi/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/KindCloud97/transactionapi/gen/proto/transactionapi"
+	"github.com/rs/zerolog/log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+func main() {
+	conn, err := grpc.Dial("transactionapi.fly.dev:80", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal().Err(err).Msg("connection failed")
+	}
+
+	client := transactionapi.NewTransactionServiceClient(conn)
+	resp, err := client.GetTransactions(context.Background(), &transactionapi.GetTransactionsRequest{
+		Id:        "",
+		To:        "",
+		BlockId:   0,
+		Timestamp: "",
+		Value:     "",
+		Gas:       "",
+		Page: &transactionapi.PageRequest{
+			Size: 3,
+			Num:  1,
+		},
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("get transactions failed")
+	}
+	fmt.Println(protojson.Format(resp))
+
+}
